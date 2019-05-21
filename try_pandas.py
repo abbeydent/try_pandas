68e58597d674a01a601c14d823131fbb0d222008
Abbey Dent <adent@g.clemson.edu>
2019-05-21 14:06:33 -0400

    Generating pandas data

---
+++ try_pandas.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import pandas as pd 
+
+
+n = 100 
+
+t = pd.date_range('20190101', periods=n)
+
+#print(t)
+
+df = pd.DataFrame(np.random.randn(n, 10), index=t, columns=list('ABCDEFGHIJ'))
+
+print(df.head())
+
+print(df.head(10))
+
+print(df.tail(5))
+
+print(df.describe())
+
+print(df.T)
+
